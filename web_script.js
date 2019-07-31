3581c57219b04a1888333be316fdb63a454a7e96
HazelTheBasil <haerae2@icloud.com>
2019-07-31 09:41:46 -0700

    project init

---
+++ web_script.js
@@ -0,0 +1,8 @@
+function bigger(){
+  var name = document.getElementById("title");
+  name.style.fontSize = "40px";
+}
+function smaller(){
+  var name = document.getElementById("title");
+  name.style.fontSize = "25px";
+}
